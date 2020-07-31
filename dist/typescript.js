5aa2105c81a7b67cdf44f17493d42e936ac7f0c3
Daniel Mamphekgo <Danielromeo99@gmail.com>
2020-07-31 21:50:09 +0200

    typescript w/ ts-node

---
+++ dist/typescript.js
@@ -0,0 +1,21 @@
+"use strict";
+// interface Person{ 
+// 	name: string;
+// 	age: number;
+// 	gender: string;
+// }
+// let isMale : boolean;
+// const greeter = (person: Person, isMale : boolean) =>  {
+//     return "Hello, " + person.name+ " AND HE IS male: "+ isMale;
+// }
+// let user = {
+// 	name : "aaaa",
+// 	age: 15,
+// 	gender: "male"
+// };
+// if(user.gender === "male"){
+// 	isMale = true;
+// }else{
+// 	isMale = false;
+// }
+// console.log(greeter(user, isMale));

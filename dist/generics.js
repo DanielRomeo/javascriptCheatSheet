6bb09ffb9ca99644447b03ada3bcf72f0ba01d8b
danielromeo <danielromeo99@gmail.com>
2020-10-13 17:22:05 +0200

    master

--- dist/generics.js
+++ dist/generics.js
@@ -7,3 +7,63 @@ function identity(arg) {
 // call it:
 let output = identity("hello world");
 console.log(output);
+// lets create another generic function that returns last item in array:
+// this is how i would do it for number:
+const last = (arr) => {
+    return arr[arr.length - 1];
+};
+// instead it should be like this:
+const last1 = (arr) => {
+    return arr[arr.length - 1];
+};
+//lets call it
+let myvalue = last1([3, 4, 5]);
+let myvalue2 = last1(["a", "c"]);
+//------------------------------------------------------------------------------------------
+// make my generic function take in different types if it needs to:
+const myfunc = (a, b) => {
+    return [a, b];
+};
+//call it
+let data = myfunc(5, "aa");
+console.log(typeof data);
+// but then it returns an array, but really its a tuple: so lets maek it return a tuple:
+const myfunc2 = (a, b) => {
+    return [a, b];
+};
+//call it:
+let data2 = myfunc2(null, "aa"); // now i know for sure that its gonna be a tuple...
+let data3 = myfunc2("alii", 100); // can sepcify before hand what exactly needs to be psssed in:
+// i can set defuly types to functions:
+// so Y will always be number:
+const myfunc3 = (a, b) => {
+    return [a, b];
+};
+//call it:
+let data4 = myfunc3("alii", 100); // as u can see, we only specified the T's type:
+// --------------------------------------------------------------------------------------
+// objects:
+// say we have a function that takes in an object, but in that object: must be atleast 2 properties... 
+// also, i want it to be able to allow an object that has more that those 2 properties...
+// the normal function that is restricted:
+const makeFullName = (obj) => {
+    return Object.assign({}, obj, { fullName: obj.firstName + " " + obj.lastName });
+};
+//use it...
+console.log(makeFullName({
+    firstName: "sam",
+    lastName: "doe"
+}));
+// real way: // meaning i have to extend the type,.,
+const makeFullName2 = (obj) => {
+    return Object.assign({}, obj, { fullName: obj.firstName + " " + obj.lastName });
+};
+// use it :
+console.log(makeFullName2({
+    firstName: "sam",
+    lastName: "doe",
+    age: 15
+}) // as u can see, we were able to add the age here:
+);
+//----------------------------------------------------------------------------------------------------------------------------------------------
+// interface, using generics with interfaces:
---
+++ dist/interfaces.js
@@ -0,0 +1,15 @@
+"use strict";
+// function that takes an object:
+function displayPersonalInfo(obj) {
+    return { obj };
+}
+let myObject = {
+    name: "daniel",
+    data: 15000
+};
+let output2 = displayPersonalInfo(myObject);
+// use it use it:
+let dansInfo = {
+    name: "danny",
+    data: 100 // have to use a number here coz thats what the type wants
+};

35c743f2def29f3c2be2d2cf1411593eeafba5e8
danielromeo <danielromeo99@gmail.com>
2020-10-17 18:16:43 +0200

    Update

--- src/classes.ts
+++ src/classes.ts
@@ -1,23 +1,24 @@
 
 
-// class Student {
-// 	fullname : string;
-// 	constructor(public firstname: string, public middlename: string, public lastname: string){
-// 		this.fullname = firstname + " " + this.middlename + " " + this.lastname;
-// 	}
-// }
+class Student {
+    fullname : string;
+    age: number;
+	constructor(public firstname: string, public middlename: string, public lastname: string){
+		this.fullname = firstname + " " + this.middlename + " " + this.lastname;
+	}
+}
 
-// interface Person {
-// 	firstname: string;
-// 	lastname: string;
-// }
+interface Person {
+	firstname: string;
+	lastname: string;
+}
 
-// const greeter = (person: Person) =>{
-// 	return `Hello ${person.firstname} ${person.lastname}`;
-// }
+const greeter = (person: Person) =>{
+	return `Hello ${person.firstname} ${person.lastname}`;
+}
 
-// let user = new Student("Daniel", "Romeo", "Mmaphekgo");
+let user = new Student("Daniel", "Romeo", "Mmaphekgo");
 
-// document.body.textContent = greeter(user);
+document.body.textContent = greeter(user);
 
-// console.log("aa");
+console.log("aa");
